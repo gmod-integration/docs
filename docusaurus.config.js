be5daf3887c36912188e1e5fdf65ca0912344abc
linventif <gregoire.becue@pm.me>
2026-01-23 10:26:50 +0100

    add edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,87 +4,85 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import { themes as prismThemes } from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer"
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Gmod Integration',
-  tagline: 'Upgrade your discord server with Gmod Integration',
-  favicon: 'img/logo.png',
+  title: "Gmod Integration",
+  tagline: "Upgrade your discord server with Gmod Integration",
+  favicon: "img/logo.png",
   // Set the production url of your site here
-  url: 'https://gmod-integration.com',
+  url: "https://gmod-integration.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'gmod-integration', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  organizationName: "gmod-integration", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"]
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.js',
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.js",
+          editUrl: "https://github.com/gmod-integration/docs/tree/main/"
         },
         pages: false,
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css"
         }
-      }),
-    ],
+      })
+    ]
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Gmod Integration',
+        title: "Gmod Integration",
         logo: {
-          alt: 'Gmod Integration Logo',
-          src: 'img/logo.png',
+          alt: "Gmod Integration Logo",
+          src: "img/logo.png"
         },
         items: [
           {
-            href: 'https://gmod-integration.com/dashboard/guilds',
-            label: 'Admin Panel',
-            position: 'right',
+            href: "https://gmod-integration.com/dashboard/guilds",
+            label: "Admin Panel",
+            position: "right"
           },
           {
-            href: 'https://gmod-integration.com/invite',
-            label: 'Invite Bot',
-            position: 'right',
+            href: "https://gmod-integration.com/invite",
+            label: "Invite Bot",
+            position: "right"
           },
           {
-            href: 'https://gmod-integration.com/workshop',
-            label: 'Workshop',
-            position: 'right',
-          },
+            href: "https://gmod-integration.com/workshop",
+            label: "Workshop",
+            position: "right"
+          }
           // {
           //   type: 'localeDropdown',
           // },
-        ],
-      },
+        ]
+      }
       //   footer: {
       //     style: 'dark',
       //     links: [
@@ -130,7 +128,7 @@ const config = {
       //     theme: prismThemes.github,
       //     darkTheme: prismThemes.dracula,
       //   },
-    }),
-};
+    })
+}
 
-export default config;
+export default config
